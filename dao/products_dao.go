250daa51c3c9323a90545d80eba81ffde15f8df9
akashgianchandani <akashg@solulab.com>
2019-03-27 18:37:34 +0530

    updated

--- dao/products_dao.go
+++ dao/products_dao.go
@@ -2,7 +2,7 @@ package dao
 
 import (
 	"fmt"
-	"github.com/akashg-solulab/golang_challenge/models"
+	. "github.com/akashg-solulab/golang_challenge/models"
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
@@ -13,15 +13,14 @@ type ProductsDAO struct {
 }
 
 var db *mgo.Database
-fmt.printf("db")
 
 const (
 	COLLECTION = "products"
 )
 
 // Establish a connection to database
-func (p *ProductsDAO) Connect() {
-	session, err := mgo.Dial(p.Server)
+func (m *ProductsDAO) Connect() {
+	session, err := mgo.Dial(m.Server)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,30 +31,4 @@ func (m *ProductsDAO) FindAll() ([]Products, error) {
 	var products []Products
 	err := db.C(COLLECTION).Find(bson.M{}).All(&products)
 	return products, err
-}
-
-
-// Find a movie by its id
-func (m *ProductsDAO) FindById(id string) (Products, error) {
-	var products Products
-	err := db.C(COLLECTION).FindId(bson.ObjectIdHex(id)).One(&products)
-	return products, err
-}
-
-// Insert a movie into database
-func (m *ProductsDAO) Insert(products Products) error {
-	err := db.C(COLLECTION).Insert(&products)
-	return err
-}
-
-// Delete an existing movie
-func (m *ProductsDAO) Delete(products Products) error {
-	err := db.C(COLLECTION).Remove(&products)
-	return err
-}
-
-// Update an existing movie
-func (m *ProductsDAO) Update(products Products) error {
-	err := db.C(COLLECTION).UpdateId(products.ID, &products)
-	return err
 }

e99e9f7f9766857afead648337498ea26b02fcfe
akashgianchandani <akashg@solulab.com>
2019-03-27 17:19:46 +0530

    updated

--- app.go
+++ app.go
@@ -17,6 +17,21 @@ import (
 var config = Config{}
 var dao = ProductsDao{}
 
+func FindProducts(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var product Products
+	if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	// if err := dao.Insert(movie); err != nil {
+	// 	respondWithError(w, http.StatusInternalServerError, err.Error())
+	// 	return
+	// }
+	fmt.Println("hello world",product)
+	
+	respondWithJson(w, http.StatusCreated, movie)
+}
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
 	respondWithJson(w, code, map[string]string{"error": msg})
@@ -35,13 +50,17 @@ func init() {
 
 	dao.Server = config.Server
 	dao.Database = config.Database
+	fmt.Println("Dao",dao)
 	dao.Connect()
 }
 
 // Define HTTP request routes
 func main() {
-	// log.Fatal("hello")
-	
-	r := mux.NewRouter()
+
+	r := mux.NewRouter();
+	r.HandleFunc("/products", FindProducts).Methods("POST")
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatal(err)
+	}
 	
 }
--- dao/products_dao.go
+++ dao/products_dao.go
@@ -1,8 +1,8 @@
 package dao
 
 import (
-	"log"
-	. "github.com/akashg-solulab/golang_challenge/models"
+	"fmt"
+	"github.com/akashg-solulab/golang_challenge/models"
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
@@ -13,16 +13,49 @@ type ProductsDAO struct {
 }
 
 var db *mgo.Database
+fmt.printf("db")
 
 const (
 	COLLECTION = "products"
 )
 
 // Establish a connection to database
-func (m *ProductsDAO) Connect() {
-	session, err := mgo.Dial(m.Server)
+func (p *ProductsDAO) Connect() {
+	session, err := mgo.Dial(p.Server)
 	if err != nil {
 		log.Fatal(err)
 	}
 	db = session.DB(m.Database)
+}
+
+func (m *ProductsDAO) FindAll() ([]Products, error) {
+	var products []Products
+	err := db.C(COLLECTION).Find(bson.M{}).All(&products)
+	return products, err
+}
+
+
+// Find a movie by its id
+func (m *ProductsDAO) FindById(id string) (Products, error) {
+	var products Products
+	err := db.C(COLLECTION).FindId(bson.ObjectIdHex(id)).One(&products)
+	return products, err
+}
+
+// Insert a movie into database
+func (m *ProductsDAO) Insert(products Products) error {
+	err := db.C(COLLECTION).Insert(&products)
+	return err
+}
+
+// Delete an existing movie
+func (m *ProductsDAO) Delete(products Products) error {
+	err := db.C(COLLECTION).Remove(&products)
+	return err
+}
+
+// Update an existing movie
+func (m *ProductsDAO) Update(products Products) error {
+	err := db.C(COLLECTION).UpdateId(products.ID, &products)
+	return err
 }
